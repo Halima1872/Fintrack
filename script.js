7d7cf1dcf588852b46ac6e4694ea6a5a85d1d730
Halima <rajwanihalima9@gmail.com>
2023-05-31 17:03:18 +0530

    Merged Expenses

--- script.js
+++ script.js
@@ -14,6 +14,13 @@ newIncomeForm = document.getElementById("newIncomeForm");
 newIncomeForm.addEventListener("submit", IncomeEvent );
 p = document.getElementById("incomeValue");
 p.append(totalIncome);
+
+totalExpense= calculateCategory('expense');
+newExpenseForm = document.getElementById("newExpenseForm");
+newExpenseForm.addEventListener("submit", ExpenseEvent );
+p = document.getElementById("expenseValue");
+p.append(totalExpense);
+
 //Function to calculate the total of the category i.e income to be displayed
 function calculateCategory(category) {
     if(getStorageItem(category + 'Categories')){
@@ -24,7 +31,6 @@ function calculateCategory(category) {
     return
 }
 }
-
 //Function to set key,value in localStorage
 function setStorageItem(key, value) {
     localStorage.setItem(key, JSON.stringify(value));
@@ -40,7 +46,13 @@ function getStorageItem(item) {
 }
 
 //Function to add income
-function addIncome(newIncome, source) {
+function Categories(category) {
+    if (getStorageItem(category + 'Categories'))
+      return getStorageItem(category + 'Categories');
+    return [];
+  }
+
+  function addIncome(newIncome, source) {
     const income = getStorageItem('incomeCategories');
     const newIncomeObj = {
         income: newIncome,
@@ -55,19 +67,12 @@ function addIncome(newIncome, source) {
     }
     location.reload()
 }
-
-function IncomeCategories() {
-    if (getStorageItem('incomeCategories'))
-      return getStorageItem('incomeCategories');
-    return [];
-  }
-
 //Function to make donut chart for income
 function addIncomeChart(){
     const labels = []
     const data = []
     const tableBody = document.getElementById("Incometablebody");
-    const incomeCategories = IncomeCategories()
+    const incomeCategories = Categories('income')
     const incomeChart = document.getElementById("incomeChart").getContext('2d')
     incomeCategories.forEach(item => {
         const tr = document.createElement('tr');
@@ -131,3 +136,87 @@ function IncomeEvent(e){
 }
 
 addIncomeChart()
+
+function addExpense(newExpense, source,date) {
+  const expense = getStorageItem('expenseCategories');
+  const newExpenseObj = {
+      expense: newExpense,
+      source: source,
+      date: date
+  };
+  if (expense !== undefined) {
+      expense.push(newExpenseObj);
+      setStorageItem('expenseCategories', expense);
+  }else{
+      setStorageItem('expenseCategories', [newExpenseObj]);
+  }
+  location.reload()
+}
+
+
+//Function to make donut chart for expense
+function addExpenseChart(){
+  const labels = []
+  const data = []
+  const tableBody = document.getElementById("Expensetablebody");
+  const expenseCategories = Categories('expense')
+  const expenseChart = document.getElementById("expenseChart").getContext('2d')
+  expenseCategories.forEach(item => {
+      const tr = document.createElement('tr');
+      tr.innerHTML = `<tr><td>-${item.source}</td><td>${item.expense}</td><td>${item.date}</td></tr>`;
+      tableBody.insertAdjacentElement('beforeend',tr)
+      labels.push(item.source)
+      data.push(item.expense)
+  });
+  const chartData = {
+      labels : labels,
+      datasets:[
+          {
+              data: data,
+              backgroundColor: [
+                  'rgb(255, 99, 132)',
+                  'rgb(255, 159, 64)',
+                  'rgb(255, 205, 86)',
+                  'rgb(75, 192, 192)',
+                  'rgb(54, 162, 235)',
+                  'rgb(1, 142, 203)',
+                  'rgb(106, 144, 204)',
+                  'rgb(1, 142, 203)',
+                  'rgb(102, 55, 221)',
+                ],
+          }
+      ]
+  }
+  const config = {
+      type: 'doughnut',
+      data: chartData,
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+            text: 'Expense Sources',
+          },
+        },
+      },
+    };
+    
+  const chart1 = new Chart(expenseChart,config)
+}
+
+function ExpenseEvent(e){
+  e.preventDefault();
+  alert("Form submitted!");
+  const newExpenseSourceInput = document.getElementById('newExpenseSourceInput').value;
+  const ExpenseAmount = document.getElementById('expenseAmount').value;
+  const ExpenseDate = document.getElementById('expenseDate').value;
+  
+  if (ExpenseAmount !== '') {
+      addExpense(ExpenseAmount, newExpenseSourceInput, ExpenseDate);
+  }
+}
+
+addExpenseChart()
